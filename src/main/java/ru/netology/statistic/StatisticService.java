7b1497074a44fb7830b59f27c754e5d37153ca17
Elenka <ilenka2687@gmail.com>
2024-05-29 16:56:42 +0300

    Version3

--- src/main/java/ru/netology/statistic/StatisticService.java
+++ src/main/java/ru/netology/statistic/StatisticService.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class StatisticService {
+package ru.netology.statistic;
+public class StatisticService {
+    public long findNax(long[] incomes){
+        long currentMax = incomes[0];
+        for (long income: incomes){
+            if (currentMax< income){
+                currentMax=income;
+            }
+        }
+        return currentMax;
+    }
 }
